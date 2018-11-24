bc0d33ba199c475f4a488c65f9f881552484fe4e
leonlau95 <leonlau95@gmail.com>
2018-11-25 00:23:01 +0800

    debug

--- app-server.js
+++ app-server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const server = express();
 const Sensor = require('./Sensor');
 const Sensor02 = require('./Sensor02');
-var value1 = 0;
-var value2 = 0;
 
 //from nodemcu
 server.get('/', (req, res) => {
   var value1 = req.query.sensor1;
   var value2 = req.query.sensor2;
 
-  const responsestr = sensor1: ${value1}, sensor2: ${value2};
+  const responsestr = "sensor1: ${value1}, sensor2: ${value2}"";
 
   res.status(200).send(responsestr);
   console.log(responsestr);
